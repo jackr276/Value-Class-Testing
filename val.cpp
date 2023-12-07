6b1fc7859b1ae2ee849825e16490b62360037888
Jack Robbins <113046361+jackr276@users.noreply.github.com>
2023-12-07 11:06:39 -0500

    rewrote functions using better logic

--- val.cpp
+++ val.cpp
@@ -10,38 +10,6 @@
 
 using namespace std;
 
-/*
-//For this operator, divide op from "this"
-Value Value::operator/(const Value& op) const{
-    //When we saw GetType(), think of it as this->GetType()
-    //If the types match continue
-    if(GetType() == op.GetType()){
-        //If we have two ints, result will be an int
-        if(IsInt()){
-            return Value(GetInt() / op.GetInt());
-        }
-        //Same thing for if they're both reals
-        if(IsReal()){
-            return Value(GetReal() / op.GetReal());
-        }
-    }
-    //Otherwise we could have a mismatch and still work
-    if (IsReal() && op.IsInt()){
-        //Cast op to be broader
-        return Value(GetReal() / (float)op.GetInt());
-    }
-
-    if (IsInt() && op.IsReal()){
-        //in this case cast the current object to be broader
-        return Value((float)GetInt() / op.GetReal());
-    }
-
-    //If we get here, we have some string or booleans, which are invalid
-    return Value();
-}
-*/
-
-
 //division may occur only between ints and reals
 Value Value::operator/(const Value& op) const{
 switch(GetType()){
@@ -77,45 +45,6 @@ switch(GetType()){
     //If we get here, op was either a string or a float and therefore invalid
     return Value();
 }
-
-/*
-//Comparing "this" with op
-Value Value::operator==(const Value& op) const {
-    //Check the cases for when types are equal first
-    if (GetType() == op.GetType()){
-        if(IsInt()){
-            return Value(GetInt() == op.GetInt());
-        }    
-
-        if(IsReal()) {
-            return Value(GetReal() == op.GetReal());
-        }
-
-        if(IsString()){
-            return Value(GetString() == op.GetString());
-        }
-
-        if(IsBool()){
-            return Value(GetBool() == op.GetBool());
-        }
-    }
-
-    //Just like the /operator, we can use casting to compare reals and ints
-    if (IsReal() && op.IsInt()){
-        //Cast op to be broader
-        return Value(GetReal() == (float)op.GetInt());
-    }
-
-    if (IsInt() && op.IsReal()){
-        //in this case cast the current object to be broader
-        return Value((float)GetInt() == op.GetReal());
-    }
-
-    //If we get here, something went wrong so just return an empty value
-    return Value();
-}
-*/
-
 
 //Comparing "this" with op
 Value Value::operator==(const Value& op) const {
